f3c3af1afa01fd4b83d3539a51eeb4718c59f0a4
brizClickBus <felipe.ribeiro@clickbus.com>
2023-01-09 06:06:38 -0300

    change base.html

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login',views.sign_in,name='login'),
+    path('',views.sign_in,name='login'),
     path('subscription/',views.sign_up,name='subscription'),
     path('sign_out/',views.sign_out,name='sign_out')
 ]
